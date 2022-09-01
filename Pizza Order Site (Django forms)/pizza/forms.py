17a9ff58aa52887e2c7af3d3bf83b11506d7b996
Anirudh Agrahari <78427532+anirudh34ag@users.noreply.github.com>
2022-09-01 19:39:50 +0530

    Add files via upload

---
+++ Pizza Order Site (Django forms)/pizza/forms.py
@@ -0,0 +1,22 @@
+from django import forms 
+from .models import Pizza, Size 
+
+#class PizzaForm(forms.Form):
+#    topping1 = forms.CharField(label = 'Topping1',max_length = 100, )
+#    topping2 = forms.CharField(label = 'Topping2',max_length = 100, ) 
+#    size = forms.ChoiceField(label = 'Size', choices = [('Small','Small'),('Medium','Medium'),('Large','Large')])
+
+class PizzaForm(forms.ModelForm):
+
+    
+
+    
+    
+    class Meta:
+        model = Pizza 
+        fields = ['topping1','topping2','size']
+        labels = {'topping1':'Topping 1','topping2':'Topping 2'}
+
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value = 2, max_value = 6)
