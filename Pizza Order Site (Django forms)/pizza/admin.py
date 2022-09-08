346ff7929f3e2f42c53c900e484a10fe5f90aa99
Anirudh Agrahari <78427532+anirudh34ag@users.noreply.github.com>
2022-09-08 20:35:26 +0530

    Delete Pizza Order Site (Django forms) directory

--- Pizza Order Site (Django forms)/pizza/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pizza, Size
-
-admin.site.register(Pizza)
-admin.site.register(Size)
-
-# Register your models here.
--- Pizza Order Site (Django forms)/pizza/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PizzaConfig(AppConfig):
-    
-    name = 'pizza'
